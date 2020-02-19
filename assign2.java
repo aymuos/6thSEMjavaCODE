0509e6c82a457ed1fb05735fff0dd132ba61eda7
aymuos <sparkacco@gmail.com>
2020-02-19 08:45:16 +0530

    Create assign2.java
    incomplete assignment2

---
+++ assign2.java
@@ -0,0 +1,19 @@
+import java.util.*;  
+class assign2   
+{  
+    public static void main(final String[] args) {
+            final String[] college = new String[5];
+            final int i=0;
+    final Scanner sc = new Scanner(System.in); // System.in is a standard input stream
+    System.out.println("Enter all the elements:");
+
+    for(int i = 0; i < college.length; i++)
+
+    {
+
+        college[i] = sc.nextInt();
+//System.out.println("Total= " +d);  
+        
+
+}  
+}  
